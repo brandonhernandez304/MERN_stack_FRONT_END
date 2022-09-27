714a701c3ae19034c259eb8970344959d1a97c1d
Brandon Hernandez <bhernandez30497@gmail.com>
2022-09-27 13:37:09 -0400

    adds heroku url to Main

--- src/components/Main.js
+++ src/components/Main.js
@@ -7,7 +7,10 @@ function Main(props) {
 
     const [people, setPeople] = useState(null)
 
-    const URL = "http://localhost:4000/people";
+    //const URL = "http://localhost:4000/people";
+
+    const URL = "https://mern-stack-bhern34.herokuapp.com/people/"
+
 
     const getPeople = async () => {
         const response = await fetch(URL)
